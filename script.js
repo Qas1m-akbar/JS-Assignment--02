40b05009b2505e3c3767b8fd4d802f3dfa00385d
Qas1m-akbar <qasimktk901@gmail.com>
2024-06-13 11:22:32 -0700

    task 02 done

--- script.js
+++ script.js
@@ -2,17 +2,32 @@
 
 
 
-let age = prompt("Enter your Age Here!");
+// let age = prompt("Enter your Age Here!");
 
-if(age<18){
-    alert("Your is not Eligible");
+// if(age<18){
+//     alert("Your is not Eligible");
+// }
+
+// else if(age>=18 && age<=50){
+//     alert("Then your are adult ,  Eligible ! ");
+// }
+
+
+// else{
+//     alert("Your are old but still Eligible!");
+// } 
+
+
+// Cheking for Even and odd Numbers//
+
+
+let num = +prompt("Enter the Number you want!")
+
+if(num%2 ==0){
+
+    alert("Then the Number is Even!");
 }
-
-else if(age>=18 && age<=50){
-    alert("Then your are adult ,  Eligible ! ");
-}
-
 
 else{
-    alert("Your are old but still Eligible!");
+       alert("The Number is Odd!");
 }
