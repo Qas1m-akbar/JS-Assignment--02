fadc78c383ae83aaf6029e6f928c328dcc013c2d
Qas1m-akbar <qasimktk901@gmail.com>
2024-06-13 11:00:21 -0700

    1 task done

---
+++ script.js
@@ -0,0 +1,18 @@
+// checking age Egibility //
+
+
+
+let age = prompt("Enter your Age Here!");
+
+if(age<18){
+    alert("Your is not Eligible");
+}
+
+else if(age>=18 && age<=50){
+    alert("Then your are adult ,  Eligible ! ");
+}
+
+
+else{
+    alert("Your are old but still Eligible!");
+}
